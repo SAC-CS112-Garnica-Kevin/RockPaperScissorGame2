647ba4c8791e17df8303ba8fd02f18cbade810f5
SAC-CS112-Garnica-Kevin <garnic18@gmail.com>
2016-03-19 23:03:37 -0700

    Update RPS.java

--- RPS.java
+++ RPS.java
@@ -9,111 +9,111 @@ class RPS
 	{
 		
 		int userRestart = 0;
-	while (userRestart == 0)	
-	{
-		Scanner input = new Scanner( System.in );
+		while (userRestart == 0)	
+		{
+			Scanner input = new Scanner( System.in );
 
-		Random generator = new Random();
-		int computerNumber;
-		int userNumber;
-		String computerSelection; 
-		String userSelection; 
+			Random generator = new Random();
+			int computerNumber;
+			int userNumber;
+			String computerSelection; 
+			String userSelection; 
 		
-		System.out.println("You're going to play a 'Rock, Paper, Scissors' game with the computer.");
-		System.out.println();
-		System.out.println("Enter one of the following numbers"); 
-		System.out.println();
-		System.out.println("1 = Rock");
-		System.out.println();
-		System.out.println("2 = Paper");
-		System.out.println();
-		System.out.println("3 or Greater = Scissors");
-		System.out.println();
+			System.out.println("You're going to play a 'Rock, Paper, Scissors' game with the computer.");
+			System.out.println();
+			System.out.println("Enter one of the following numbers"); 
+			System.out.println();
+			System.out.println("1 = Rock");
+			System.out.println();
+			System.out.println("2 = Paper");
+			System.out.println();
+			System.out.println("3 or Greater = Scissors");
+			System.out.println();
 
-		userNumber = input.nextInt();
+			userNumber = input.nextInt();
 		
-		computerNumber = generator.nextInt(3)+1;
+			computerNumber = generator.nextInt(3)+1;
 		
-		if(computerNumber == 1){
+			if(computerNumber == 1){
 			
-			computerSelection = "Rock";
-		}else if (computerNumber == 2){
+				computerSelection = "Rock";
+			}else if (computerNumber == 2){
 			
-			computerSelection = "Paper";
-		}else {
+				computerSelection = "Paper";
+			}else {
 			
-			computerSelection = "Scissors";
-		};
+				computerSelection = "Scissors";
+			};
 		
-		if(userNumber == 1){
+			if(userNumber == 1){
 			
-			userSelection = "Rock";
+				userSelection = "Rock";
 			
-		}else if (userNumber == 2){
+			}else if (userNumber == 2){
 			
-			userSelection = "Paper";
+				userSelection = "Paper";
 			
-		}else {
-			
-			userSelection = "Scissors";
-		};
+			}else {
+				
+				userSelection = "Scissors";
+			};
 		
-		System.out.println();
+			System.out.println();
 		
-		if (computerNumber == userNumber){
+			if (computerNumber == userNumber){
 			
-			System.out.println("Both of you have chosen " + userSelection);
-			System.out.println("The Game is a Tie");
+				System.out.println("Both of you have chosen " + userSelection);
+				System.out.println("The Game is a Tie");
 			
-		}else if (computerNumber == 1 && userNumber >= 3){
+			}else if (computerNumber == 1 && userNumber >= 3){
 			
-			System.out.println("The Cpu has chosen " + computerSelection + ".");
-			System.out.println("You have chosen " + userSelection);
-			System.out.println("You Loose");
+				System.out.println("The Cpu has chosen " + computerSelection + ".");
+				System.out.println("You have chosen " + userSelection);
+				System.out.println("You Loose");
 			
-		}else if (computerNumber == 2 && userNumber == 1){
+			}else if (computerNumber == 2 && userNumber == 1){
 			
-			System.out.println("The Cpu has chosen " + computerSelection + ".");
-			System.out.println("You have chosen " + userSelection);
-			System.out.println("You Loose");
+				System.out.println("The Cpu has chosen " + computerSelection + ".");
+				System.out.println("You have chosen " + userSelection);
+				System.out.println("You Loose");
 			
-		}else if (computerNumber == 3 && userNumber == 2){
+			}else if (computerNumber == 3 && userNumber == 2){
 			
-			System.out.println("The Cpu has chosen " + computerSelection + ".");
-			System.out.println("You have chosen " + userSelection);
-			System.out.println("You Loose");
+				System.out.println("The Cpu has chosen " + computerSelection + ".");
+				System.out.println("You have chosen " + userSelection);
+				System.out.println("You Loose");
 		
-		}else if (computerNumber == 1 && userNumber == 2){
+			}else if (computerNumber == 1 && userNumber == 2){
 			
-			System.out.println("The Cpu has chosen " + computerSelection + ".");
-			System.out.println("You have chosen " + userSelection);
-			System.out.println("You Win");
+				System.out.println("The Cpu has chosen " + computerSelection + ".");
+				System.out.println("You have chosen " + userSelection);
+				System.out.println("You Win");
 			
-		}else if (computerNumber == 2 && userNumber >= 3){
+			}else if (computerNumber == 2 && userNumber >= 3){
 			
-			System.out.println("The Cpu has chosen " + computerSelection + ".");
-			System.out.println("You have chosen " + userSelection);
-			System.out.println("You Win");
+				System.out.println("The Cpu has chosen " + computerSelection + ".");
+				System.out.println("You have chosen " + userSelection);
+				System.out.println("You Win");
 			
-		}else if (computerNumber == 3 && userNumber == 1){
+			}else if (computerNumber == 3 && userNumber == 1){
 			
-			System.out.println("The Cpu has chosen " + computerSelection + ".");
-			System.out.println("You have chosen " + userSelection);
-			System.out.println("You Win");
+				System.out.println("The Cpu has chosen " + computerSelection + ".");
+				System.out.println("You have chosen " + userSelection);
+				System.out.println("You Win");
 		
-		}else {
+			}else {
 			
-			System.out.println("Error");
+				System.out.println("Error");
 			
-		};
+			};
 	
-		System.out.println("Would you like to play again?");
-		System.out.println();
-		System.out.println("0 = Yes");
-		System.out.println();
-		System.out.println("1 = No");
-		userRestart = input.nextInt();
-	}
+			System.out.println("Would you like to play again?");
+			System.out.println();
+			System.out.println("0 = Yes");
+			System.out.println();
+			System.out.println("1 = No");
+			userRestart = input.nextInt();
+		}
 		
 		
 	}
